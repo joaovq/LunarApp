fdfe0fea62f9fb58e80d1e6872371fd084979d6d
joaovq <joaovqdev@gmail.com>
2025-04-13 11:42:38 -0300

    feature: add logs to articles paging

--- app/src/main/java/br/com/joaovq/lunarappcompose/data/network/paging/ArticlesPagingSource.kt
+++ app/src/main/java/br/com/joaovq/lunarappcompose/data/network/paging/ArticlesPagingSource.kt
@@ -4,16 +4,20 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import br.com.joaovq.lunarappcompose.data.network.dto.ArticleDto
 import br.com.joaovq.lunarappcompose.data.network.service.SpaceFlightNewsApi
+import timber.log.Timber
 
 class ArticlesPagingSource(
     private val query: String?,
     private val backend: SpaceFlightNewsApi,
 ) : PagingSource<Int, ArticleDto>() {
+    private val log = Timber.tag(this::class.java.simpleName)
     override suspend fun load(
         params: LoadParams<Int>
     ): LoadResult<Int, ArticleDto> {
         try {
             val nextPageNumber = params.key ?: 0
+            log.d("articles page key: $nextPageNumber")
+            log.d("articles page load size: ${params.loadSize}")
             val response = backend.getArticles(
                 query = query,
                 limit = params.loadSize,
--- app/src/main/java/br/com/joaovq/lunarappcompose/presentation/viewmodel/ArticlesViewModel.kt
+++ app/src/main/java/br/com/joaovq/lunarappcompose/presentation/viewmodel/ArticlesViewModel.kt
@@ -4,19 +4,28 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import br.com.joaovq.lunarappcompose.data.network.datasource.SpaceFlightRemoteDataSource
+import br.com.joaovq.lunarappcompose.di.annotations.IODispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onEmpty
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class ArticlesViewModel @Inject constructor(
     spaceFlightApiRemoteDataSource: SpaceFlightRemoteDataSource,
-    dispatcher: CoroutineDispatcher = Dispatchers.IO
+    @IODispatcher dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
+    private val log = Timber.tag(this::class.java.simpleName)
     val articles = spaceFlightApiRemoteDataSource
         .getArticles()
         .cachedIn(viewModelScope)
+        .onEach { log.d("articles fetched: $it") }
+        .catch { log.d("error occurred: ${it.message}") }
+        .onEmpty { log.d("articles list is empty") }
         .flowOn(dispatcher)
 }
